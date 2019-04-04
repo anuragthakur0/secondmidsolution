46bb924fd9ac6eacdf5a33559922af2d98b5ab97
anuragthakur0 <49266039+anuragthakur0@users.noreply.github.com>
2019-04-04 12:26:33 +0530

    secondmid first question

---
+++ stringmethod.py
@@ -0,0 +1,11 @@
+a='Chirag'
+print('string',end=' ')
+print(a)
+print('string as title',end='  ')
+print(a.title())
+print('count ir in string',end=' ')
+print(a.count('ir'))
+print('string in lowercase',end=' ')
+print(a.lower())
+print('swapcase of string',end=' ')
+print(a.swapcase())
